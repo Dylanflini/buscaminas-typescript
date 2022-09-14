bf3d1d50cfda96e3c4257b75c9e2ff838254eaa2
Daniel Páez <daniel.paez@globant.com>
2022-09-13 21:50:58 -0300

    chore: ignore public folder

---
+++ public/game-board/infrastructure/data/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// import { GameBoardRepository } from '@domain/game-board-repository'
+// export const dataRepository: GameBoardRepository = {
+// 	saveGameBoard: async () => {
+// 		return {
+// 			cells: [
+// 				{ x: 0, y: 0 },
+// 				{ x: 1, y: 0 },
+// 				{ x: 0, y: 1 },
+// 				{ x: 1, y: 1 },
+// 			],
+// 			totalMines: 4,
+// 			remainingMines: 4,
+// 			hasWonGame: false,
+// 			hasLostGame: false,
+// 		}
+// 	},
+// }
