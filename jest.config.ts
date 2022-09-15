92c9c6506c1aed128b227688d7a0a3dee98707a5
Dylanflini <dyl.camilo@gmail.com>
2022-09-15 15:12:49 -0300

    changed roots of tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from '@jest/types'
-import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from './tsconfig.json'
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config.InitialOptions = {
-	watch: false,
-	// testEnvironment: 'jsdom',
-	moduleNameMapper: {
-		...pathsToModuleNameMapper(compilerOptions.paths, {
-			prefix: '<rootDir>/',
-		}),
-	},
-	transform: {
-		'^.+\\.ts?$': 'ts-jest',
-	},
-}
+  watch: false,
+  roots: ['minesweeper'],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+  },
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
 
-export default config
+export default config;
