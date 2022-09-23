0d52623d40eaafabfce4417cd200507124e9289e
Dylanflini <dyl.camilo@gmail.com>
2022-09-23 19:23:29 -0300

    refactor: change routes handler from object to array

--- src/minesweeper/infrastructure/server/utils/routerHandler.ts
+++ src/minesweeper/infrastructure/server/utils/routerHandler.ts
@@ -2,21 +2,38 @@ import { IncomingMessage, ServerResponse } from 'http';
 import { RequestListener } from '../types';
 import { ServerError, ServerErrorMessages } from './validations';
 
+enum Methods {
+  GET = 'GET',
+  POST = 'POST',
+}
+
+interface Route {
+  method: Methods;
+  route: string;
+  callback: RequestListener;
+}
+
+type RouteHandler = (route: string, callback: RequestListener) => void;
+type ExecRoute = (req: IncomingMessage, res: ServerResponse) => Promise<void>;
+
 export const Router = () => {
-  const routes: Record<any, RequestListener> = {};
+  const routes: Route[] = [];
 
-  const get = (route: string, callback: RequestListener) => {
-    routes[`GET-${route}`] = callback;
-  };
-  const post = (route: string, callback: RequestListener) => {
-    routes[`POST-${route}`] = callback;
+  const get: RouteHandler = (route, callback) => {
+    routes.push({ method: Methods.GET, route, callback });
   };
 
-  const execRoute = async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
-    const foundRoute = routes[`${req.method || ''}-${req.url || ''}`];
+  const post: RouteHandler = (route, callback) => {
+    routes.push({ method: Methods.POST, route, callback });
+  };
 
-    if (typeof foundRoute === 'function') {
-      return foundRoute(req, res);
+  const execRoute: ExecRoute = async (req, res) => {
+    const foundRoute = routes.find(
+      ({ method, route }) => method === req.method && route === req.url,
+    );
+
+    if (foundRoute) {
+      return foundRoute.callback(req, res);
     }
 
     throw new ServerError(404, ServerErrorMessages.NOT_FOUND);
