01960307454a38643c449bf7dabe0bb1d3c5627c
Dylan Muñoz <42789063+Dylanflini@users.noreply.github.com>
2022-09-25 00:54:18 -0300

    fix: error when create cells

--- src/minesweeper/use-cases/startGame/startGame.spec.ts
+++ src/minesweeper/use-cases/startGame/startGame.spec.ts
@@ -70,10 +70,10 @@ describe('start game', () => {
     expect(cells).toStrictEqual([
       new Cell([0, 0]),
       new Cell([1, 0]),
-      new Cell([2, 0]),
       new Cell([0, 1]),
       new Cell([1, 1]),
-      new Cell([2, 1]),
+      new Cell([0, 2]),
+      new Cell([1, 2]),
     ]);
   });
 
