4635460deec71ef56ad9a3029c655ab64e9958cc
Dylan Muñoz <42789063+Dylanflini@users.noreply.github.com>
2022-10-01 12:55:05 -0300

    added endpoint constants

--- src/minesweeper/infrastructure/controllers/startGame/startGame.controller.spec.ts
+++ src/minesweeper/infrastructure/controllers/startGame/startGame.controller.spec.ts
@@ -1,12 +1,33 @@
 import request from 'supertest';
 import { createServer } from 'http';
 import { requestHandler } from '@minesweeper/infrastructure/server/requestHandler';
+import { endpoints } from '@minesweeper/infrastructure/server/constants';
+
+/*
+
+Requerimientos:
+  - debe contener el search param de bombs
+  - debe contener el search param de rows
+  - debe contener el search param de columns
+
+  -los 3 parametros deben ser de tipo numero
+  -los 3 parametros no deben ser un NaN
+
+*/
 
 describe('start game controller', () => {
+  it('should throw error if not bombs is provided as parameter', () => {
+    const server = createServer();
+
+    // const response = await request(server).get(`${endpoints.game}?bombs=1&rows=2&columns=2`);
+
+    // expect()
+  });
+
   it('should return correct data', async () => {
     const server = createServer(requestHandler);
 
-    const response = await request(server).get('/start?bombs=1&rows=2&columns=2');
+    const response = await request(server).get(`${endpoints.game}?bombs=1&rows=2&columns=2`);
 
     expect(response.statusCode).toBe(200);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
@@ -23,7 +44,7 @@ describe('start game controller', () => {
   it('should return Bad Request error if client send wrong data', async () => {
     const server = createServer(requestHandler);
 
-    const response = await request(server).get('/start?bombs=1&rows=0&columns=2');
+    const response = await request(server).get(`${endpoints.game}?bombs=1&rows=0&columns=2`);
 
     expect(response.statusCode).toBe(400);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
---
+++ src/minesweeper/infrastructure/server/constants.ts
@@ -0,0 +1,5 @@
+const APP_NAME = 'minesweeper';
+
+export const endpoints = {
+  game: `/${APP_NAME}/game`,
+};
